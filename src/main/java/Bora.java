0ab60a0802b18f74bacf75941c3f2648edcd854b
Bora Simsek <97452363+borasimsek@users.noreply.github.com>
2024-03-19 15:27:26 +0300

    Some Minor Changes
    Nothing new

--- src/main/java/Bora.java
+++ src/main/java/Bora.java
@@ -1,7 +1,10 @@
+import List.DoublyNode;
+
 public class Bora {
     public static void main(String[] args) {
         System.out.println("Selam");
         System.out.println("Bora");
+        DoublyNode node = new DoublyNode(5);
     }
 
 }
